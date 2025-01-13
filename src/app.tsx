193879c6f8e4175df3a258171ae2d6bf33d56e4d
Drew <sirampr@gmail.com>
2025-01-13 17:25:57 -0500

    add editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "./components/app-sidebar";
+import { Editor } from "./components/editor";
 import { NotesList } from "./components/notes-list";
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
       <div className="p-6">
         <h1 className="font-semibold text-xl">Project Ideas</h1>
         <h2 className="font-light text-sm text-gray-700">Jan 5, 2025</h2>
+        <Editor />
       </div>
     </>
   );
